b12722c09bf26cdb01590fe267642182eb27eb51
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-28 21:52:22 +0900

    [fix]

--- app/controllers/api/v1/tinder_controller.rb
+++ app/controllers/api/v1/tinder_controller.rb
@@ -37,12 +37,28 @@ Capybara.javascript_driver = :remote_chrome
 
 class Api::V1::TinderController < ApplicationController
   include Capybara::DSL
-  before_action :client, only: [:callback]
+  # before_action :client, only: [:callback]
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def callback
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    }
+    @body = request.body.read
+    @signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless @client.validate_signature(@body, @signature)
+      error 400 do
+        'Bad Request'
+      end
+    end
+    @line_header = {
+      'Authorization' => "Bearer " + ENV['LINE_CHANNEL_TOKEN'],
+      'Content-type' => 'application/json',
+    }
     events = @client.parse_events_from(@body)
     events.each do |event|
       case event
