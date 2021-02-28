9a48811a9f9f1805bcf5d1db0de4e5641c4cc7ea
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-28 20:51:18 +0900

    Revert "[fix]seedの修正"
    This reverts commit 1e0653fa19f430b44bfc50a4d29821c8e1d6f86b.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@user = Tinder.create!(access_token:"111111111111")
+@user = User.create!(access_token:"111111111111")
